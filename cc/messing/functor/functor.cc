e5abc17ccee7e7c03bea9747dd357052ef723e22
rich@wight <r.j.henry@gmail.com>
2023-02-09 16:07:22 +0000

    Added a functor example and a lambda too.

--- cc/messing/functor/functor.cc
+++ cc/messing/functor/functor.cc
@@ -7,11 +7,14 @@
 
 #include "functor.h"
 
+static void increment_array();
+
 int main(int argc, const char * argv[]) {
 
     std::cout << "Hello, Functor!" << std::endl;
     
     functors();
+    increment_array();
     
     return 0;
 }
@@ -19,4 +22,32 @@ int main(int argc, const char * argv[]) {
 void functors() {
     
     std::cout << "in functors()" << std::endl;
+}
+
+// MARK: -
+
+static int increment(int x) {  return (x + 1); }
+
+class incrementer {
+
+    int inc;
+ 
+public:
+    incrementer(int n) : inc(n) { std::cout << "incrementer constructed with " << n << std::endl; }
+    
+    int operator () (int num) const { return num + inc; }
+};
+
+void increment_array() {
+    
+    int arr[] = { -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5 };
+    
+    // Use a function to increment it
+    std::transform(arr, arr + std::size(arr), arr, increment);
+    // Use a functor to do the same thing
+    std::transform(arr, arr + std::size(arr), arr, incrementer(1));
+    // Use a lambda to do the same thing
+    std::transform(arr, arr + std::size(arr), arr, [](int x) { return x + 1; });
+
+    for (auto i : arr) std::cout << i << ", ";
 }
