bf2355be22646471d783602baebffd9f1953b03b
rich@wight <r.j.henry@gmail.com>
2023-02-16 12:17:14 +0000

    Split out lambda thread

--- cc/messing/threads/basics.cpp
+++ cc/messing/threads/basics.cpp
@@ -14,13 +14,20 @@
 using namespace std::chrono;
 using namespace std::chrono_literals;
 
+static void thread_with_lambda(int reps);
+
 void basic_thread_test() {
+    
+    thread_with_lambda(30);
+}
+
+static void thread_with_lambda(int reps) {
     
     auto main_thread = std::this_thread::get_id();
     
-    auto lambda = [main_thread](std::string name) {
+    auto lambda = [main_thread, reps](std::string name) {
         std::cout << name << " thread id : " << std::this_thread::get_id() << " (main ID : " << main_thread << ")" << std::endl;
-        for (auto _ = 3; _--;) {
+        for (auto _ = reps; _--;) {
             std::cout << name << " starting sleeping" << std::endl;
             std::this_thread::sleep_for(1s);
             std::cout << name << " finished sleeping" << std::endl;
