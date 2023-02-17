50e76a91e7aad882e98b2b43bba85964e3e2f653
rich@wight <r.j.henry@gmail.com>
2023-02-17 10:50:50 +0000

    Rot13 got a  makefile.

---
+++ cc/state_mess/state_mess/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  state_mess
+//
+//  Created by Richard Henry on 17/02/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
